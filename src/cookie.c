3ba41cd71ad83129867ce2672534c7a21f731634
Paweł Tarczykowski <farrel1@wp.pl>
2013-02-25 12:14:48 +0100

    Fix variable declaration

--- src/cookie.c
+++ src/cookie.c
@@ -4,16 +4,16 @@
 
 #include "cookie.h"
 
-struct VoyCookie* voy_cookie_parse(gchar const *cookie_string) {
-  struct VoyCookie* cookie;
+struct VoyCookie *voy_cookie_parse(gchar const *cookie_string) {
+  struct VoyCookie *cookie;
   cookie = calloc(sizeof (struct VoyCookie), 1);
   gchar **splitted;
   int i = 0;
-  gchar* token;
+  gchar *token;
   splitted = g_strsplit(cookie_string, "; ", -1);
   token = splitted[i];
   while (token != NULL) {
-    gchar** key_value = g_strsplit(token, "=", 2);
+    gchar **key_value = g_strsplit(token, "=", 2);
     voy_cookie_set_value(cookie, key_value[0], key_value[1]);
     g_free(key_value[0]);
     g_free(key_value[1]);
@@ -26,9 +26,9 @@ struct VoyCookie* voy_cookie_parse(gchar const *cookie_string) {
   return cookie;
 }
 
-gchar* voy_cookie_print(struct VoyCookie const *cookie) {
-  gchar* result;
-  gchar* tmp;
+gchar *voy_cookie_print(struct VoyCookie const *cookie) {
+  gchar *result;
+  gchar *tmp;
   result = g_strconcat(cookie->name, "=", cookie->value, NULL);
   if (cookie->domain != NULL) {
     tmp = result;
@@ -41,7 +41,7 @@ gchar* voy_cookie_print(struct VoyCookie const *cookie) {
     g_free(tmp);
   }
   if (cookie->expires != NULL) {
-    gchar* date_time = g_date_time_format(cookie->expires, VOY_COOKIE_TIME_FORMAT);
+    gchar *date_time = g_date_time_format(cookie->expires, VOY_COOKIE_TIME_FORMAT);
     tmp = result;
     result = g_strconcat(tmp, "; Expires=", date_time, NULL);
     g_free(date_time);
@@ -60,7 +60,7 @@ gchar* voy_cookie_print(struct VoyCookie const *cookie) {
   return result;
 }
 
-void voy_cookie_set_value(struct VoyCookie *cookie, gchar * const key, gchar * const value) {
+void voy_cookie_set_value(struct VoyCookie *cookie, gchar const *key, gchar const *value) {
   if (g_strcmp0(key, "Domain") == 0) {
     if (cookie->domain != NULL) g_free(cookie->domain);
     cookie->domain = g_strdup(value);
@@ -91,7 +91,7 @@ void voy_cookie_print_screen(struct VoyCookie const *cookie) {
     g_print("\tvalue: %s\n", cookie->value);
   }
   if (cookie->expires != NULL) {
-    gchar* date_time = g_date_time_format(cookie->expires, VOY_COOKIE_TIME_FORMAT);
+    gchar *date_time = g_date_time_format(cookie->expires, VOY_COOKIE_TIME_FORMAT);
     g_print("\texpires: %s\n", date_time);
     g_free(date_time);
   }
@@ -109,7 +109,7 @@ void voy_cookie_print_screen(struct VoyCookie const *cookie) {
   }
 }
 
-struct GDateTime* voy_cookie_parse_expires_date(char* const date_string) {
+struct GDateTime *voy_cookie_parse_expires_date(char const *date_string) {
   struct tm tm;
   strptime(date_string, VOY_COOKIE_TIME_FORMAT, &tm);
   struct GDateTime *date_time = g_date_time_new(g_time_zone_new("Z"), tm.tm_year + 1900,
--- src/cookie.h
+++ src/cookie.h
@@ -6,21 +6,21 @@
 char *VOY_COOKIE_TIME_FORMAT = "%a, %d-%b-%Y %H:%M:%S GMT";
 
 typedef struct VoyCookie{
-  gchar* name;
-  gchar* value;
-  gchar* domain;
-  gchar* path;
-  GDateTime* expires;
+  gchar *name;
+  gchar *value;
+  gchar *domain;
+  gchar *path;
+  GDateTime *expires;
   gboolean secure;
   gboolean http_only;
 } VoyCookie;
 
-extern struct VoyCookie* voy_cookie_parse(gchar const *cookie_string);
-extern gchar* voy_cookie_print(struct VoyCookie const *cookie);
-extern void voy_cookie_set_value(struct VoyCookie *cookie, gchar * const key, gchar * const value);
+extern struct VoyCookie *voy_cookie_parse(gchar const *cookie_string);
+extern gchar *voy_cookie_print(struct VoyCookie const *cookie);
+extern void voy_cookie_set_value(struct VoyCookie *cookie, gchar const * key, gchar const * value);
 extern void voy_cookie_print_screen(struct VoyCookie const *cookie);
-extern struct GDateTime* voy_cookie_parse_expires_date(gchar* const date_string);
-extern void voy_cookie_free(struct VoyCookie* cookie);
+extern struct GDateTime* voy_cookie_parse_expires_date(gchar const *date_string);
+extern void voy_cookie_free(struct VoyCookie *cookie);
 
 #endif	/* VOY_COOKIE_H */
 
